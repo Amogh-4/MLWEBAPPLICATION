d5b12f7b5b8bb11eaf45d29d748f170101647e72
Amogh-4 <126194553+Amogh-4@users.noreply.github.com>
2023-09-06 02:28:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+f = open('iris (1).pkl', 'rb')
+model = pickle.load(f)
+f.close()
+
+app = Flask(__name__)
+
+@app.route('/')
+def man():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+def home():
+    SL = request.form['a']
+    SW = request.form['b']
+    PL = request.form['c']
+    PW = request.form['d']
+    arr = np.array([[float(SL), float(SW), float(PL), float(PW)]])
+    pred = model.predict(arr)
+    return render_template('after.html', data=pred)
+
+if __name__ == '__main__':
+    app.run(debug = True)
